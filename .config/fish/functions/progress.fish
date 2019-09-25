f96ee3474d79e9f45f3f72696035d978eecc8cb0
Tushar Jois <16180220+tusharjois@users.noreply.github.com>
2019-09-25 09:14:19 -0400

    updated dotfiles

--- .config/fish/functions/progress.fish
+++ .config/fish/functions/progress.fish
@@ -6,6 +6,7 @@ function progress
     end
     vim +'normal Go' +'r!date' +'s/^/## /' +'noh' ~/progress/$argv.md
     pushd ~/progress
+    git pull origin master 2> /dev/null > /dev/null
     git add $argv.md
     git commit -m "updated progress for $argv" > /dev/null
     and git push origin master 2> /dev/null
